e76403e8c8f7200fc9f36a452e61d638d728a4fa
denkersdaan-ops <denkersdaan@gmail.com>
2026-05-06 12:02:49 +0200

    added a header

---
+++ html/header.php
@@ -0,0 +1,14 @@
+<header class="blue">
+    <img src="images/logo.jpg" alt="Logo" class="logo">
+    <form action="">
+        <input type="text" placeholder="Search...">
+        <button type="submit">Search</button>
+    </form>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
--- html/index.php
+++ html/index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+    <?php include_once 'header.php'; ?>
     Hallo
 </body>
 </html>
