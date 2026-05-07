25499c0020401f2b98cd4e98a88f7e88dceef06a
denkersdaan-ops <denkersdaan@gmail.com>
2026-05-07 13:46:20 +0200

    added search bar to header.php

--- html/header.php
+++ html/header.php
@@ -1,8 +1,8 @@
 <header class="blue">
-    <img src="images/logo.jpg" alt="Logo" class="logo">
-    <form action="">
-        <input type="text" placeholder="Search...">
-        <button type="submit">Search</button>
+    <img src="images/logo.jpg" alt="Logo" class="main-logo">
+    <form action="" method="get" class="search-form">
+        <input type="text" name="search" id="search-input" class="yellow" placeholder="Search...">
+        <button type="submit" id="search-btn" hidden>Search</button>
     </form>
     <nav>
         <ul>
@@ -12,3 +12,19 @@
         </ul>
     </nav>
 </header>
+
+<script>
+    // Get the input field
+var input = document.getElementById("search-input");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("search-btn").click();
+  }
+});
+</script>
